f4b1b7c7e5d5f3e59a2e86b1dd05911f616b53d6
danrocus1994 <danrodcastillo1994@gmail.com>
2019-10-27 14:25:12 -0500

    keep pushing

--- holberton.h
+++ holberton.h
@@ -10,11 +10,18 @@ int print_string(va_list *);
 int print_decimal(va_list *);
 int (*handler(char c))(va_list *);
 char *_tostring(int n);
+/**
+*struct printers - printer functions
+*@joker: his name
+*@fun:his age
+*Description: my little baby awesome dog
+*/
+
 typedef struct printers
 {
 	char joker;
 	int (*fun)(va_list *);
-}print_t;
+} print_t;
 print_t *get_funs();
 
 #endif  /* HOL_PRINTF */
