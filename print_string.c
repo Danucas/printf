f2a1e2a4c919a525ab18f94a1b18391ef8430ade
danrocus1994 <danrodcastillo1994@gmail.com>
2019-10-28 12:36:22 -0500

    keep pushing

--- print_string.c
+++ print_string.c
@@ -13,11 +13,10 @@ int print_string(va_list *list)
 	char  *s  = va_arg(*list, char *);
 	int count = 0;
 
-/*	if (s == NULL)
+	if (s == NULL)
 	{
-		write(1, "(null)", 6);
-		return (6);
-		}*/
+		_put(*(s));
+	}
 	while (*(s + count) != '\0')
 	{
 		_put(*(s + count));
