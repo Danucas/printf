761636639462d757174d2c34ec68285153f3ddd9
danrocus1994 <danrodcastillo1994@gmail.com>
2019-10-28 12:10:01 -0500

    keep pushing

--- printf.c
+++ printf.c
@@ -11,26 +11,28 @@ int _printf(const char *format, ...)
 {
 	va_list op, *ap;
 	unsigned int char_c = 0;
-	int i = 0, forml = 0, flags = 0;
-	int (*function)(va_list *);
+	int i = 0, forml = 0, flags = 0, (*function)(va_list *);
 
 	va_start(op, format);
 	ap = &op;
 	if (!format || (format[1] == '\0' && format[0] == '%'))
 		return (-1);
 	while (format[forml] != '\0')
-	{
 		forml++;
-	}
 	while (format[i] != '\0')
 	{
 		if (format[i] == '%')
 		{
 			function = handler(format[i + 1]);
+			if (function == NULL)
+				i++;
+			else
+			{
 			char_c += function(ap);
 			flags += 1;
 			i += 2;
-		}
+			}
+			}
 		if (i <= forml)
 		{
 			if (format[i] == '\0')
