87d0b1602c122971c8a5db4f1fb8474c79ea5d7c
danrocus1994 <danrodcastillo1994@gmail.com>
2019-10-29 01:50:57 -0500

    dsad

--- test/1-main.c
+++ test/1-main.c
@@ -9,22 +9,36 @@ int main(void)
    int len2;
    len = _printf(NULL);
    len2 = printf(NULL);
-_printf("%d\n", INT_MIN);
-printf("%d\n", INT_MIN);
-_printf("%d\n", INT_MAX);
-printf("%d\n", INT_MAX);
-_printf("%d\n", UINT_MAX);
-printf("%d\n", UINT_MAX);
+len = _printf("%d\n", INT_MIN);
+len2 = printf("%d\n", INT_MIN);
 _printf("Length:[%d, %i]\n", len, len);
 printf("Length:[%d, %i]\n", len2, len2);
-_printf("Negative:[%d]\n", -762534);
-printf("Negative:[%d]\n", -762534);
+len = _printf("%d\n", INT_MAX);
+len2 = printf("%d\n", INT_MAX);
+_printf("Length:[%d, %i]\n", len, len);
+printf("Length:[%d, %i]\n", len2, len2);
+len = _printf("%d\n", UINT_MAX - 1);
+len2 = printf("%d\n", UINT_MAX - 1);
+_printf("Length:[%d, %i]\n", len, len);
+printf("Length:[%d, %i]\n", len2, len2);
+len = _printf("Negative:[%d]\n", -762534);
+len2 = printf("Negative:[%d]\n", -762534);
+_printf("Length:[%d, %i]\n", len, len);
+printf("Length:[%d, %i]\n", len2, len2);
+len = _printf("%d\n", 0);
+   len2 = printf("%d\n", 0);
+_printf("Length:[%d, %i]\n", len, len);
+printf("Length:[%d, %i]\n", len2, len2);
+   len = _printf("%d\n", -1);
+   len2 = printf("%d\n", -1);
+_printf("Length:[%d, %i]\n", len, len);
+printf("Length:[%d, %i]\n", len2, len2);
 
-_printf("%d\n", 0);
-   printf("%d\n", 0);
-   _printf("%d\n", -1);
-   printf("%d\n", -1);
-   printf("%d %d\n", len, len2);
+   len = _printf("%d\n", 1);
+   len2 = printf("%d\n", 1);
+_printf("Length:[%d, %i]\n", len, len);
+printf("Length:[%d, %i]\n", len2, len2);
+
    return (0);
 
 }
--- tostring.c
+++ tostring.c
@@ -1,45 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
+/**
+ *get_len - print strings
+ *@n: the list
+ *@factor: das
+ *@res: dsa
+ *@tosum: dsa
+ *@j:dsa
+ *@sign:dsa
+ *Return: char count
+ */
+
+int get_len(int *factor, int *res, int *n, int *tosum, int *j, char *sign)
+{
+	int i = 0;
+
+	if (*n < 0)
+	{
+		if (*n <= INT_MIN)
+			*res -= 1;
+		else
+			*res *= -1;
+	}
+	*res /= 10;
+	for (i = 1; *res > 0; i++)
+	{
+		*factor *= 10;
+		*res /= 10;
+	}
+	if (*n < 0)
+	{
+		if (*n <= INT_MIN)
+		{
+			*n = (*n + 1) * -1;
+			*tosum = 1;
+		}
+		else
+		{
+			*n *= -1;
+		}
+		*sign = '-';
+		i++;
+		*j = 1;
+	}
+	else
+	{
+		*j = 0;
+	}
+	return (i);
+}
+/**
+ *_tostring - print strings
+ *@n: the list
+ *Return: char count
+ */
 
 char *_tostring(int n)
 {
 	int factor = 1, i, j, res = n, tosum = 0;
 	char *ch, sign;
 
-
-	if (n < 0)
-	{
-		if (n <= INT_MIN)
-			res -= 1;
-		else
-			res *= -1;
-	}
-	res /= 10;
-	for(i = 1; res > 0; i++)
-	{
-		factor *= 10;
-		res /= 10;
-	}
-	if (n < 0)
-	{
-		if (n <= INT_MIN)
-		{
-			n = (n + 1) * -1;
-			tosum = 1;
-		}
-		else
-		{
-			n *= -1;
-		}
-		sign = '-';
-		i++;
-		j = 1;
-	}
-	else
-	{
-		j = 0;
-	}
+	i = get_len(&factor, &res, &n, &tosum, &j, &sign);
 	ch = malloc((i + 1) * sizeof(char));
 	if (ch == NULL)
 		return (NULL);
@@ -47,7 +69,7 @@ char *_tostring(int n)
 	{
 		ch[0] = sign;
 	}
-	for( j = j; j <= i; j++)
+	for (j = j; j <= i; j++)
 	{
 		if (n > 9)
 		{
@@ -69,13 +91,10 @@ char *_tostring(int n)
 			j++;
 		}
 		else
-		{
 			ch[j] =  '0' + res;
-		}
 		if (j == i - 1 && tosum != 0)
-			ch[j] = '0'+ (res + tosum);
+			ch[j] = '0' + (res + tosum);
 	}
 	ch[i] = '\0';
-
-	return(ch);
+	return (ch);
 }
