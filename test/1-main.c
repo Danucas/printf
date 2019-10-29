d65acaa1634fa43e071f0ce64f660d819016a3af
danrocus1994 <danrodcastillo1994@gmail.com>
2019-10-28 22:17:08 -0500

    keep pushing

---
+++ test/1-main.c
@@ -0,0 +1,24 @@
+#include <limits.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include "holberton.h"
+int main(void)
+{
+   int len;
+   int len2;
+   len = _printf(NULL);
+   len2 = printf(NULL);
+_printf("%d\n", INT_MIN);
+printf("%d\n", INT_MIN);
+_printf("%d\n", INT_MAX);
+printf("%d\n", INT_MAX);
+_printf("%d\n", UINT_MAX);
+printf("%d\n", UINT_MAX);
+
+_printf("%d\n", 0);
+   printf("%d\n", 0);
+   printf("%d %d\n", len, len2);
+   return (0);
+
+}
